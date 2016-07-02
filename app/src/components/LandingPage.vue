017e23a1b72cb305a5be0528b78b193e10ab5cc1
Arthur Longbottom <art.longbottom.jr@gmail.com>
2016-07-02 05:14:10 -0700

    added placeholder cards for selection

--- app/src/components/LandingPage.vue
+++ app/src/components/LandingPage.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="page-content">LandingPage</div>
+  <div class="page-content">
+    <div v-for="n in 4" class="demo-card-square mdl-card mdl-shadow--2dp">
+      <div class="mdl-card__title mdl-card--expand">
+        <h2 class="mdl-card__title-text">Camp Pendleton</h2>
+      </div>
+      <div class="mdl-card__supporting-text">
+        Insert address here
+      </div>
+      <div class="mdl-card__actions mdl-card--border">
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+          Enter Clinic
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +23,19 @@
 </script>
 
 <style scoped>
-
+  .page-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    height: calc(100vh - 56px);
+  }
+  .demo-card-square.mdl-card {
+    width: 200px;
+    height: 320px;
+  }
+  .demo-card-square > .mdl-card__title {
+    color: #fff;
+    background:
+      url('http://placekitten.com/320/320') bottom right 15% no-repeat #46B6AC;
+  }
 </style>
